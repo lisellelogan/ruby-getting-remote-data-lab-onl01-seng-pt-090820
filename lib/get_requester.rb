50d9b4225fd3e27599249266e43a7423b5c024f7
Liselle Logan <liselle.logan@gmail.com>
2020-10-10 02:29:21 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,7 +4,9 @@ class GetRequester
     @url = url
   end
 
-  def get_response_body
-    
+  def get_response_body(json_string)
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response_body(uri)
+    response.body
   end
 end
