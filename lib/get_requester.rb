c7cb069cc69171580d12cf75c0e5593854101bf2
Liselle Logan <liselle.logan@gmail.com>
2020-10-10 02:36:18 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,15 +3,15 @@ require 'pry'
 class GetRequester
   require 'open-uri'
   require 'net/http'
-  
+
   def initialize(url)
     @url = url
   end
 
   def get_response_body
     uri = URI.parse(self)
-    binding.pry
-    response = Net::HTTP.get_response(uri)
-    response.body
+    # binding.pry
+    # response = Net::HTTP.get_response(uri)
+    # response.body
   end
 end
